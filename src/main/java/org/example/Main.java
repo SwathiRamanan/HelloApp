1c352a0357844444a43067883450ff4d9cc70925
SwathiRamanan <sr3638@srmist.edu.in>
2026-04-10 13:51:14 +0530

    Added UC3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,11 @@
 package org.example;
-public class Main {
-    // Note: The main method MUST include (String[] args) to accept arguments
-    public static void main(String[] args) {
 
-        // Check if an argument was actually provided to avoid errors
+public class Main {
+    public static void main(String[] args) {
+        String name = "World";
         if (args.length > 0) {
-            // Get the name from the first command-line argument
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            // Fallback message if no name is provided
-            System.out.println("Hello World!");
+            name = args[0];
         }
+        System.out.println("Hello, " + name + "!");
     }
 }
